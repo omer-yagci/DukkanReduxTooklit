aa5f8d47d109a4c3c135846780708add668d2c1e
khan <omeryagci3295@gmail.com>
2022-10-27 18:06:26 +0300

    cart component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
